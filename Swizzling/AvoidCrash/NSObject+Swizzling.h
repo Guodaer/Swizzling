a401b615272cc66d96475fa2c2a1cfc0eeb3c1ae
guoda <guoda@moqing.com>
2018-02-26 14:55:34 +0800

    添加注释

--- Swizzling/AvoidCrash/NSObject+Swizzling.h
+++ Swizzling/AvoidCrash/NSObject+Swizzling.h
@@ -10,6 +10,20 @@
 
 @interface NSObject (Swizzling)
 
+/**
+ 交换实例方法
+ 
+ @param originalSelector 原方法
+ @param swizzledSelector 处理过的方法
+ */
 + (void)swizzleSelector:(SEL)originalSelector withSwizzledSelector:(SEL)swizzledSelector;
+
+/**
+ 交换类方法
+ 
+ @param originalSelector 系统原始方法
+ @param swizzledSelector 处理过的方法
+ */
++ (void)swizzle_Class_Selector:(SEL)originalSelector withSwizzledSelector:(SEL)swizzledSelector;
 
 @end
